11903a906a00c282c57e15949a22c8f1422ef9d1
Hiroaki_Iida <iida.hiroaki@entcomp.esys.tsukuba.ac.jp>
2017-01-21 10:22:02 +0900

    シーン遷移概ね 画像の大きさ，クリックでのメッセージ進み要修正

--- Assets/HasebeAssets/Script/Player_Controller.cs
+++ Assets/HasebeAssets/Script/Player_Controller.cs
@@ -1,13 +1,18 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Player_Controller : MonoBehaviour
 {
     public float vel=0.10f;
     public float right=5f;
-    public float left=5f;
-    public int stress_point;
+    public float left= -5f;
+    public static int stress_point;
+    public int add_stress_point = 10;
+    //遷移したい任意のシーン名を入力する
+    public string scene;
+
     // Use this for initialization
     void Start()
     {
@@ -21,12 +26,13 @@ public class Player_Controller : MonoBehaviour
         {
             transform.position += transform.forward * vel;
         }
-        if (Input.GetKey("d"))
+        else if (Input.GetKey("d"))
         {
             transform.Rotate(0, right, 0);
         }
-        if (Input.GetKey("a"))
+        else if (Input.GetKey("a"))
         {
+            Debug.Log(left);
             transform.Rotate(0, left, 0);
         }
     }
@@ -34,8 +40,10 @@ public class Player_Controller : MonoBehaviour
     {
         if (other.gameObject.tag == "NPC")
         {
-            stress_point++;
+            stress_point += add_stress_point;
             Debug.Log(stress_point);
+            if(stress_point>100) SceneManager.LoadScene("Ending");
+
         }
     }
 }
