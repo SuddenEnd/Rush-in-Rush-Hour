3580a33421c18f6bfbf53ca4b65bf99c4b1704d5
Hiroto Yokota <m011446489@edu.teu.ac.jp>
2017-01-22 00:39:28 +0900

    電車(断面）モデル追加

--- Assets/HasebeAssets/Script/mainLoadScript.cs
+++ Assets/HasebeAssets/Script/mainLoadScript.cs
@@ -13,16 +13,8 @@ public class mainLoadScript : MonoBehaviour
     string now_lang = Change_lang_Button.now_lang;
     Button btn;
 
-<<<<<<< HEAD
-    //public void Update()
-    //{
-    //    if (now_lang == "Japanese")
-    //    {
-    //        btn = GetComponent<Button>();
-    //        GetComponentInChildren<Text>().text = "START";
-    //    }
-    //}
-=======
+
+
     private void Start()
     {
         SMO = GameObject.Find("SoundManager").GetComponent<SoundManager_origin>();
@@ -36,7 +28,7 @@ public class mainLoadScript : MonoBehaviour
             GetComponentInChildren<Text>().text = "START";
         }
     }
->>>>>>> origin/CHIBA
+
 
     public void LoadMain()
     {
