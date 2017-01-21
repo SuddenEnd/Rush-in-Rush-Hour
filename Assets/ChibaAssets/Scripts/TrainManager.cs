ec228cdb5c09d4237959ff9d11e01c356e2b5003
CHIBA <bigwave_whale@yahoo.co.jp>
2017-01-21 23:59:45 +0900

    処理速度高速化

--- Assets/ChibaAssets/Scripts/TrainManager.cs
+++ Assets/ChibaAssets/Scripts/TrainManager.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class TrainManager : MonoBehaviour {
     public List<GameObject> TrainList = new List<GameObject>();
-    [Range(1, 10)]
+    [Range(0, 10)]
     public int trainLength;
     private List<Animator> animatorList = new List<Animator>();
     private bool isClose;
@@ -12,19 +12,28 @@ public class TrainManager : MonoBehaviour {
     public float doorLimit;
     private float doorLimit_memory;
     public GameObject trainSet;
+    public int trainCount;
+    private int trainCount_memory;
 
     // Use this for initialization
     void Start () {
         isClose = true;
-        for (int i=0; i < trainLength; i++) {
+        for (int i=0; i < trainCount; i++) {
             CreateTrain(i);
         }
         doorLimit_memory = doorLimit;
         trainSet = GameObject.Find("TrainSet");
-	}
-	
-	// Update is called once per frame
-	void Update () {
+        trainCount_memory = trainCount;
+    }
+
+    // Update is called once per frame
+    void Update () {
+        if (trainCount != trainCount_memory) {
+            CreateTrain(trainCount - 1);
+        }
+
+        trainCount_memory = trainCount;
+
         if (Input.GetKeyDown(KeyCode.O))
             TrainDoorOpen();
         if (!isClose) {
@@ -38,12 +47,13 @@ public class TrainManager : MonoBehaviour {
         }
 	}
 
-    void CreateTrain(int Distance) {
+    public void CreateTrain(int Distance) {
         int trainRandom = Random.Range (0, TrainList.Count);
         GameObject trainObject = Instantiate(TrainList[trainRandom]) as GameObject;
-        trainObject.transform.position = new Vector3(0, 0, 13.6f * Distance);
         animatorList.Add(trainObject.transform.FindChild("DoorSet").GetComponent<Animator>());
         trainObject.transform.parent = trainSet.transform;
+        trainObject.transform.position = new Vector3(0, 0, 13.6f * Distance);
+        trainObject.GetComponent<TrainStatus>().ID = Distance;
     }
 
     public void TrainDoorOpen() {
