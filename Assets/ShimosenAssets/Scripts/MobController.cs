a1046a69df3dd4f81fe27ad3f9f7ddec46559ce2
shimoaraiso <wataru.shimoaraiso@ssie.jp>
2017-01-22 04:51:35 +0900

    Mobが乗り込んだ後の挙動を安定化

--- Assets/ShimosenAssets/Scripts/MobController.cs
+++ Assets/ShimosenAssets/Scripts/MobController.cs
@@ -8,21 +8,20 @@ public class MobController : MonoBehaviour {
 	private GameObject target;
 	private Transform myTfm;
 	private Vector3 newTargetPos;
-	private bool isRide;
+	private bool isRide1;
 	private bool isRide2;
 	private float speed;
-	private bool inTheTrain;
 
 	private UnityEngine.AI.NavMeshAgent agent;
 
 	void Start() {
-		speed = 5.0f;
+		speed = 2.0f;
 		myTfm = transform;
 		targets = GameObject.FindGameObjectsWithTag("Target");
-
 		// 一番距離の近いターゲットを取得する
-		float sqrDist = 99.9f;	// 暫定の遠い値
+		float sqrDist = 999.9f;	// 暫定の遠い値
 		foreach (GameObject target in targets) {
+		//Debug.Log(target.name);
 			if (((myTfm.position - target.transform.position).sqrMagnitude) < sqrDist) {
 				this.target = target;
 				sqrDist = (myTfm.position - target.transform.position).sqrMagnitude;
@@ -34,11 +33,13 @@ public class MobController : MonoBehaviour {
 
 	void Update() {
 		// 第1目的地へ
-		if (isRide) {
+		if (isRide1) {
+			//Debug.Log("hoge1");
 			agent.SetDestination(target.transform.position);
 		}
 		// 第2目的地へ
 		if (isRide2) {
+			//Debug.Log("hoge2");
 			myTfm.position = Vector3.MoveTowards(myTfm.position, newTargetPos, speed * Time.deltaTime);
 		}
 
@@ -50,32 +51,33 @@ public class MobController : MonoBehaviour {
 
 	// 電車に乗り込み開始するメソッド
 	public void RideTrain() {
-		isRide = true;
+		isRide1 = true;
 		agent.enabled = true;
 		//myTfm.SetParent(null);
 	}
 
 	void OnCollisionEnter(Collision col) {
+		if (!isRide2) {
+			// 最初のターゲットに接触したら、第2目的地へ
+			if (col.gameObject.CompareTag("Target")) {
 
-		// 最初のターゲットに接触したら、第2目的地へ
-		if (col.gameObject.CompareTag("Target")) {
+				isRide1 = false;
+				isRide2 = true;
+				agent.enabled = false;
 
-			isRide2 = true;
+				// 電車内の範囲を設定
+				float x = Random.Range(-0.8f, 0.65f);
+				float y = Random.Range(0.5f, 0.5f);
+				float z = Random.Range(-6.5f, 6.5f);
 
-			// 電車内の範囲を設定
-			float x = Random.Range(-0.8f, 0.65f);
-			float y = Random.Range(0.5f, 0.5f);
-			float z = Random.Range(-6.5f, 6.5f);
-			
-			// 第2目的の座標を生成
-			newTargetPos = new Vector3(x, y, z);
-		}
+				// 第2目的の座標を生成
+				newTargetPos = new Vector3(x, y, z);
+			}
 
-		// 乗り込んだMobは車両の子要素になる
-		if (col.gameObject.CompareTag("Stage")) {
-			inTheTrain = true;
-			myTfm.SetParent(col.transform.parent.parent);
-			//Debug.Log(col.transform.parent.parent.name);
+			// 乗り込んだMobは車両の子要素になる
+			if (col.gameObject.CompareTag("Stage")) {
+				myTfm.SetParent(col.transform.parent.parent);
+			}
 		}
 	}
 
