dace1da572caede9949d1ca143cafeccd517d5f1
shimoaraiso <wataru.shimoaraiso@ssie.jp>
2017-01-21 16:38:15 +0900

    乗車人数変数化

--- Assets/ShimosenAssets/Scripts/Platform.cs
+++ Assets/ShimosenAssets/Scripts/Platform.cs
@@ -4,17 +4,41 @@ using UnityEngine;
 
 public class Platform : MonoBehaviour {
 
+	private GameObject[] aligns;
+	private List<GameObject> sortedAligns = new List<GameObject>();
 	private GameObject mobObj;
 	private Transform myTfm;
 	private Animator myAnim;
-
 	private bool isScroll;
+	private int mobNum;
+
+	// 乗り込む人数（最大40）
+	private int MobNum {
+		get {
+			return this.mobNum;
+		}
+		set {
+			this.mobNum = Mathf.Clamp(value, 0, 40);
+		}
+	}
 
 	void Start () {
+		MobNum = 10;
+
 		mobObj = Resources.Load("Mob") as GameObject;
 		myTfm = transform;
 		myAnim = GetComponent<Animator>();
 
+		aligns = GameObject.FindGameObjectsWithTag("Alignment");
+
+		for (int num = 1; num <= 5; num++) {
+			foreach (GameObject align in aligns) {
+				int i = int.Parse(align.name.Substring(9));
+				if (i == num) {
+					sortedAligns.Add(align);
+				}
+			}
+		}
 	}
 
 	void Update () {
@@ -23,7 +47,7 @@ public class Platform : MonoBehaviour {
 			if (isScroll) {
 				isScroll = false;
 			} else {
-				//isScroll = true;
+				isScroll = true;
 				Invoke("SpawnAtAlignment", 1.0f);
 
 			}
@@ -33,12 +57,15 @@ public class Platform : MonoBehaviour {
 	}
 
 	void SpawnAtAlignment() {
-		GameObject[] aligns = GameObject.FindGameObjectsWithTag("Alignment");
 
-		foreach (GameObject align in aligns) {
+		int count = 0;
+		foreach (GameObject align in sortedAligns) {
 			GameObject mob = Instantiate(mobObj, align.transform.position, Quaternion.identity, myTfm);
-			//Debug.Log(mob.transform.position);
-			//mob.transform.SetParent(myTfm, true);
+			count++;
+
+			if (count >= mobNum) {
+				break;
+			}
 		}
 	}
 }
