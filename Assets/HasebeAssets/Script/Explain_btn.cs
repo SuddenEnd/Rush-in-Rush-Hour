1efa953ecae6c1b113db6c84bf92817d9e43c62b
Hiroaki_Iida <iida.hiroaki@entcomp.esys.tsukuba.ac.jp>
2017-01-21 19:17:05 +0900

    言語切替実装中 とりあえず途中経過 あまり変更はないはず

---
+++ Assets/HasebeAssets/Script/Explain_btn.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+
+public class Explain_btn : MonoBehaviour {
+
+    //遷移したい任意のシーン名を入力する
+    public string scene;
+ //   string now_lang = now_lang;
+    Button btn;
+
+    public void Update()
+    {
+        if (Change_lang_Button.now_lang == "English")
+        {
+            btn = GetComponent<Button>();
+            GetComponentInChildren<Text>().text = "EXPLANATION";
+        }
+        if (Change_lang_Button.now_lang == "Japanese")
+        {
+            btn = GetComponent<Button>();
+            GetComponentInChildren<Text>().text = "説明";
+        }
+    }
+
+    public void LoadMain()
+    {
+        SceneManager.LoadScene(scene);
+    }
+
+}
+
--- Assets/HasebeAssets/Script/mainLoadScript.cs
+++ Assets/HasebeAssets/Script/mainLoadScript.cs
@@ -9,10 +9,16 @@ public class mainLoadScript : MonoBehaviour
 {
     //遷移したい任意のシーン名を入力する
     public string scene;
+    string now_lang = Change_lang_Button.now_lang;
+    Button btn;
 
-    public void onClick()
+    public void Update()
     {
-        GetComponent<Button>().onClick.AddListener(LoadMain);
+        if (now_lang == "Japanese")
+        {
+            btn = GetComponent<Button>();
+            GetComponentInChildren<Text>().text = "START";
+        }
     }
 
     public void LoadMain()
