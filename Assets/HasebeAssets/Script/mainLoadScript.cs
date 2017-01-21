bb28bbddafd097bdbca2c77fff2d949a0b7db94d
Hiroto Yokota <m011446489@edu.teu.ac.jp>
2017-01-22 04:45:17 +0900

    UI配置その1

--- Assets/HasebeAssets/Script/mainLoadScript.cs
+++ Assets/HasebeAssets/Script/mainLoadScript.cs
@@ -10,9 +10,8 @@ public class mainLoadScript : MonoBehaviour
     SoundManager_origin SMO;
     //遷移したい任意のシーン名を入力する
     public string scene;
-<<<<<<< HEAD
+
     string now_lang = Change_lang_Button.now_lang;
-    Button btn;
 
     //public void Update()
     //{
@@ -22,12 +21,12 @@ public class mainLoadScript : MonoBehaviour
     //        GetComponentInChildren<Text>().text = "START";
     //    }
     //}
-=======
+
     Image btn;
     public Sprite japanese, english;
 
 
->>>>>>> origin/Haseve
+
     private void Start()
     {
         SMO = GameObject.Find("SoundManager").GetComponent<SoundManager_origin>();
