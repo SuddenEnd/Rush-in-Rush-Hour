5cdd7970a5c5eb440c7ba440da23f46564679bf7
CHIBA <bigwave_whale@yahoo.co.jp>
2017-01-22 14:07:15 +0900

    no message

--- Assets/HasebeAssets/Script/EndingScenario.cs
+++ Assets/HasebeAssets/Script/EndingScenario.cs
@@ -7,6 +7,7 @@ using UnityEngine.SceneManagement;
 
 public class EndingScenario : MonoBehaviour
 {
+    SoundManager_origin SMO;
     public AdvEngine engine;
     string scenarioLabel;
     //遷移したい任意のシーン名を入力する
@@ -15,26 +16,25 @@ public class EndingScenario : MonoBehaviour
     // Use this for initialization
     void Start()
     {
+        SMO = GameObject.Find("SoundManager").GetComponent<SoundManager_origin>();
         int resultStresspoint = Player_Controller.stress_point;
 
         //stress値によってエンディング変化
         if(Change_lang_Button.now_lang == "Japanese") {
-            if (Player_Controller.isLeftGameOver) scenarioLabel = "GameOver_left";
-            else if (TimeManager.TimeUpflag) scenarioLabel = "GameOver_timeup";
-            else if (resultStresspoint >= 1000) scenarioLabel = "GameOver_stress";
-            else if (resultStresspoint > 600) scenarioLabel = "Badend";
-            else if (resultStresspoint > 400) scenarioLabel = "Nomalend";
-            else scenarioLabel = "Goodend";
+            if (TimeManager.TimeUpflag) { scenarioLabel = "GameOver_timeup"; SMO.SE_Shot(3); }
+            else if (resultStresspoint >= 1000) { scenarioLabel = "GameOver_stress"; SMO.SE_Shot(3); }
+            else if (resultStresspoint > 600) { scenarioLabel = "Badend"; SMO.SE_Shot(3); }
+            else if (resultStresspoint > 400) { scenarioLabel = "Nomalend"; SMO.SE_Shot(3); }
+            else { scenarioLabel = "Goodend"; SMO.SE_Shot(2); }
         }
 
         else
         {
-            if (Player_Controller.isLeftGameOver) scenarioLabel = "GameOver_left_Eng";
-            else if (TimeManager.TimeUpflag) scenarioLabel = "GameOver_timeup_Eng";
-            else if (resultStresspoint >= 1000) scenarioLabel = "GameOver_stress_Eng";
-            else if (resultStresspoint > 600) scenarioLabel = "Badend_Eng";
-            else if (resultStresspoint > 400) scenarioLabel = "Nomalend_Eng";
-            else scenarioLabel = "Goodend_Eng";
+            if (TimeManager.TimeUpflag) { scenarioLabel = "GameOver_timeup_Eng"; SMO.SE_Shot(3); }
+            else if (resultStresspoint >= 1000) { scenarioLabel = "GameOver_stress_Eng"; SMO.SE_Shot(3); }
+            else if (resultStresspoint > 600) { scenarioLabel = "Badend_Eng"; SMO.SE_Shot(3); }
+            else if (resultStresspoint > 400) { scenarioLabel = "Nomalend_Eng"; SMO.SE_Shot(3); }
+            else { scenarioLabel = "Goodend_Eng"; SMO.SE_Shot(2); }
         }
 
         StartCoroutine(CoTalk());
