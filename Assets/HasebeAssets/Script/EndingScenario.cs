69daa67649e548a3cb6b3ed96a38d23945344945
Hiroaki_Iida <iida.hiroaki@entcomp.esys.tsukuba.ac.jp>
2017-01-22 11:37:17 +0900

    取り急ぎエンディング分岐条件をプレイヤーのストレス耐性に合わせ10倍 GameOverを別としてフラグで管理するか検討

--- Assets/HasebeAssets/Script/EndingScenario.cs
+++ Assets/HasebeAssets/Script/EndingScenario.cs
@@ -20,9 +20,9 @@ public class EndingScenario : MonoBehaviour
         //stress値によってエンディング変化
         if(Change_lang_Button.now_lang == "Japanese") {
             if (TimeManager.TimeUpflag) scenarioLabel = "GameOver_timeup";
-            else if (resultStresspoint >= 100) scenarioLabel = "GameOver_stress";
-            else if (resultStresspoint > 60) scenarioLabel = "Badend";
-            else if (resultStresspoint > 40) scenarioLabel = "Nomalend";
+            else if (resultStresspoint >= 1000) scenarioLabel = "GameOver_stress";
+            else if (resultStresspoint > 600) scenarioLabel = "Badend";
+            else if (resultStresspoint > 400) scenarioLabel = "Nomalend";
             else scenarioLabel = "Goodend";
         }
 
